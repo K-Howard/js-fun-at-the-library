dfc1b094378116b7c911ecfcc9273274320056e9
Kyle Howard <kylehoward1228@yahoo.com>
2022-02-03 09:38:17 -0700

    started on shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,10 @@
-
+function shelfBook(book, shelf) {
+ if (shelf.length < 3) {
+  shelf.unshift(book)
+ }
+}
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
