aaf2d9b80a44ae20d679e98434445dc01b71f21f
Kyle Howard <kylehoward1228@yahoo.com>
2022-02-06 19:55:49 -0700

    complete iteration 2

--- src/shelf.js
+++ src/shelf.js
@@ -3,9 +3,28 @@ function shelfBook(book, shelf) {
   shelf.unshift(book)
  }
 }
+function unshelfBook(name, shelf) {
+  var bookByName = (book) => book.title == name;
+  var bookIndex = shelf.findIndex(bookByName);
+  shelf.splice(bookIndex, 1);
+}
+function listTitles(shelf) {
+ var titlesArray = []
+ for (var i = 0; i < shelf.length; i++) {
+   titlesArray.push(shelf[i].title)
+ }
+ return titlesArray.join(', ')
+}
+function searchShelf(shelf, title) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title)
+  }
+  return titles.includes(title)
+}
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
